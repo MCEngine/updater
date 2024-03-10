00691ec60c7b044f5d6c0b728e0ec5ebd49f3359
owen <56456776+JetsadaWijit@users.noreply.github.com>
2024-03-10 09:26:26 +0000

    set up

---
+++ spigot/src/main/java/com/github/mcengine/MCEngineUpdater.java
@@ -0,0 +1,11 @@
+package com.github.mcengine;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MCEngineProfile extends JavaPlugin {
+    @Override
+    public void onEnable() {}
+
+    @Override
+    public void onDisable() {}
+}
