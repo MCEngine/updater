11e4e951c67e3afd489707e441c04537791941cf
JetsadaWijit <Ze_ro_owen@hotmail.com>
2024-05-10 10:20:19 +0000

    Finished code

--- spigot/src/main/java/com/github/mcengine/MCEngineUpdater.java
+++ spigot/src/main/java/com/github/mcengine/MCEngineUpdater.java
@@ -1,5 +1,8 @@
 package com.github.mcengine;
 
+import java.util.List;
+import java.util.ArrayList;
+
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class MCEngineUpdater extends JavaPlugin {
@@ -10,7 +13,14 @@ public class MCEngineUpdater extends JavaPlugin {
             @Override
             public void run() {
                 try {
-                    GitHub.run("MCEngine", "mcengine"); // Access the static method in a static way
+                    List<String> datas = new ArrayList<>();
+                    datas = Util.readFile("GitHub");
+                    for (String data: datas) {
+                        String[] parts = data.split("/");
+                        String owner = parts[0];
+                        String repo = parts[1];
+                        GitHub.run(owner, repo);
+                    }
                 } catch (Exception e) {
                     System.out.println(e.getMessage());
                 }
