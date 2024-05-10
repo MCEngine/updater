e74a1f6cb8c6281c43d35e6aa6082617a575df23
JetsadaWijit <Ze_ro_owen@hotmail.com>
2024-05-10 06:32:28 +0000

    Create "Scheduled task"

--- spigot/src/main/java/com/github/mcengine/MCEngineUpdater.java
+++ spigot/src/main/java/com/github/mcengine/MCEngineUpdater.java
@@ -2,9 +2,20 @@ package com.github.mcengine;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import com.github.mcegnine.util;
+
 public class MCEngineUpdater extends JavaPlugin {
+
     @Override
-    public void onEnable() {}
+    public void onEnable() {
+        // Scheduled task to run every sunday 00:00AM
+        getServer().getScheduler().scheduleSyncRepeatingTask(this, new Runnable() {
+            @Override
+            public void run() {
+                util.readfile("github");
+            }
+        }, 0L, 20L * 60);
+    }
 
     @Override
     public void onDisable() {}
