d970edad16d14759fcf2e483f2c41f66f154eab7
JetsadaWijit <Ze_ro_owen@hotmail.com>
2024-05-10 10:07:57 +0000

    Change code to example

--- spigot/src/main/java/com/github/mcengine/MCEngineUpdater.java
+++ spigot/src/main/java/com/github/mcengine/MCEngineUpdater.java
@@ -3,14 +3,17 @@ package com.github.mcengine;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class MCEngineUpdater extends JavaPlugin {
-
     @Override
     public void onEnable() {
         // Scheduled task to run every sunday 00:00AM
         getServer().getScheduler().scheduleSyncRepeatingTask(this, new Runnable() {
             @Override
             public void run() {
-                Util.readfile("github");
+                try {
+                    GitHub.run("MCEngine", "mcengine"); // Access the static method in a static way
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                }
             }
         }, 0L, 20L * 60);
     }
