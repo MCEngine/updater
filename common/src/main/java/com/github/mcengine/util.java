6136cbfa93ec85ffc11bf8b1d8e5bf240b283240
JetsadaWijit <Ze_ro_owen@hotmail.com>
2024-05-10 06:26:54 +0000

    Craete util.java

---
+++ common/src/main/java/com/github/mcengine/util.java
@@ -0,0 +1,31 @@
+package com.github.mcengine;
+
+import java.io.File;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class util {
+    public static String getPath() {
+        return System.getProperty("user.dir") + "/plugins/Updater";
+    }
+
+    public static void readfile(String provider) {
+        File file = new File(getPath() + "/" + provider + ".properties");
+        if (!file.exists()) {
+            System.out.println("File not found");
+            return;
+        }
+
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+            String line;
+            int count = 1;
+            while ((line = br.readLine()) != null) {
+                System.out.println(count + ":" + line);
+                count++;
+            }
+        } catch (IOException e) {
+            System.out.println("Error reading file: " + e.getMessage());
+        }
+    }
+}
