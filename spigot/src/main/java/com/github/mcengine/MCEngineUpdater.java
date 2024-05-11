da62ebf0a58d3c7af4909b7b4f866d97dcaedd63
JetsadaWijit <Ze_ro_owen@hotmail.com>
2024-05-11 00:51:18 +0000

    code for get "tag_latest"

--- spigot/src/main/java/com/github/mcengine/MCEngineUpdater.java
+++ spigot/src/main/java/com/github/mcengine/MCEngineUpdater.java
@@ -19,9 +19,23 @@ public class MCEngineUpdater extends JavaPlugin {
                         datas = Util.readFile(provider);
                         for (String data: datas) {
                             String[] parts = data.split(":");
+
+                            // Data format: owner:repo:tag
                             String owner = parts[0];
                             String repo = parts[1];
                             String tag_current = parts[2];
+
+                            Class clazz = Class.forName("com.github.mcengine." + provider);
+
+                            String tag_latest = (String) clazz.getMethod("getLatestTag", String.class, String.class).invoke(null, owner, repo);
+
+                            if (Util.isNewerVersion(tag_current, tag_latest)) {
+                                // Notify user about new version
+                                System.out.println("Owner : " + owner);
+                                System.out.println("Repo : " + repo);
+                                System.out.println("Current : " + tag_current);
+                                System.out.println("Latest : " + tag_latest);
+                            }
                         }
                     }
                 } catch (Exception e) {
